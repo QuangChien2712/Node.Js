8099a258a3ee1fecc36a8c545870e8e64ce0104a
Chien <cvoquang@gmail.com>
2022-02-19 23:09:08 +0700

    video-9 connect database

---
+++ src/configs/connectDB.js
@@ -0,0 +1,50 @@
+// get the client
+// const mysql = require('mysql2');
+// import mysql from 'mysql2';
+
+// // create the connection to database
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'nodejsbasic'
+// });
+
+// // simple query
+// connection.query(
+//     'SELECT * FROM `users`',
+//     function(err, results, fields) {
+//         console.log("Checkout sucess");
+//         console.log(results); // results contains rows returned by server
+//         console.log(fields); // fields contains extra meta data about results, if available
+//     }
+// );
+
+// export default connection;
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "nodejsbasic",
+    port: 3306,
+    charset: "utf8mb4"
+});
+
+// connection.connect(err => {
+//     if (!err) {
+//         console.log("DB Connection Succeeded");
+//     } else {
+//         console.log("DB Connection Failed");
+//     }
+// });
+connection.query(
+    'SELECT * FROM `users`',
+    function(err, results, fields) {
+        console.log("Checkout sucess");
+        console.log(results); // results contains rows returned by server
+        // console.log(fields); // fields contains extra meta data about results, if available
+    }
+);
+
+module.exports = connection;
