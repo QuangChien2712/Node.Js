828494c5e998d44ff12f70439c54b56083cef60f
Chien <cvoquang@gmail.com>
2022-02-19 17:40:39 +0700

    video-8

--- src/server.js
+++ src/server.js
@@ -1,23 +1,15 @@
 // const express = require('express')
 import express from 'express'
 import configViewEngine from './configs/viewEngine'
+import initWebRoute from './route/web'
 require('dotenv').config()
 
 // const path = require('path')
 const app = express()
 const port = process.env.PORT || 8080
 
-configViewEngine(app)
-
-
-app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, "./index.html"))
-    res.render("index.ejs")
-})
-
-app.get('/about', (req, res) => {
-    res.send('Toi la Chien!')
-})
+configViewEngine(app);
+initWebRoute(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
