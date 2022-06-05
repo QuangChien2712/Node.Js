edda95b0736c58ec5ebb680182606ddae0b492f6
Chien <cvoquang@gmail.com>
2022-06-05 17:41:16 +0700

    FullStack NodeJs Video 68 (#59) Edit Gender -> String, update migration and database

--- src/services/userService.js
+++ src/services/userService.js
@@ -108,8 +108,9 @@ let createNewUser = (data) => {
                     lastName: data.lastName,
                     address: data.address,
                     phoneNumber: data.phonenumber,
-                    gender: data.gender === "1" ? true : false,
+                    gender: data.gender,
                     roleId: data.roleId,
+                    positionId: data.positionId
                 });
                 resolve({
                     errCode: 0,
